00e77f3d48d48d3090cdfb8c7b0d0e55641b7381
shamaz332 <shamazsaeed332@gmail.com>
2020-11-07 22:23:39 +0500

    netlify lambda func assesed in index.js

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,10 +1,10 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
+const handler = async (event,context) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Hello from function${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
 import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  const [data, setData] = useState("")
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(`/.netlify/functions/hello`)
+      const tempData = await response.json()
+      setData(tempData)
+    })()
+  }, [])
+
+  return (
+  <div>
+    <h1>hi</h1>
+    {data.message}
+    
+    </div>
+  )}
